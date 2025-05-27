ed45b65c21a4d3e4db050d1aa69d3d0b639ac5a4
Shanmukha Naga Suresh Gandham <85335126+SureshGandham18@users.noreply.github.com>
2025-05-27 15:30:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 4000
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://food-del-frontend-p976.onrender.com/", // your actual frontend domain
+  credentials: true
+}))
+
 
 // db connection
 connectDB();
